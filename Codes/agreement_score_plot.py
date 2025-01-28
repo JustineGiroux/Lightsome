94895c70c2ab6908e6a402f14497d58bd396d4c1
Justine Giroux <50334118+JustineGiroux@users.noreply.github.com>
2025-01-28 11:53:25 -0500

    Fixed bugs related to paths

--- Codes/agreement_score_plot.py
+++ Codes/agreement_score_plot.py
@@ -7,6 +7,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+root = Path(__file__).resolve().parents[1]
+
 """ 
     Plots figure 5 in the paper, showing the agreement score between the metrics and the observers, for all the experiments.
 """
@@ -45,13 +47,13 @@ method_list_paths_ordered = ['gt_indoor', 'weber22', 'everlight', 'stylelight', 
 method_list_outdoor_paths_ordered = ['gt_outdoor', 'everlight_outdoor', 'jinsong_outdoor', 'average_image_outdoor_texture']
 
 ## Path des renders indoor
-render_path = Path(f"../Data/Renders/renders_indoor")
+render_path = root / f"Data/Renders/renders_indoor"
 ## Liste des noms des images
 temp_path = render_path / type_of_blender_scenes[0] / "gt_indoor" / material_list[0]
 input_im_names_indoor = sorted([f for f in os.listdir(temp_path) if f.endswith('.exr')])
 
 ## Path des renders outdoor
-render_path = Path(f"../Data/Renders/renders_outdoor")
+render_path = root / f"Data/Renders/renders_outdoor"
 ## Liste des noms des images
 temp_path = render_path / type_of_blender_scenes[0] / "gt_outdoor" / material_list[0]
 input_im_names_outdoor = sorted([f for f in os.listdir(temp_path) if f.endswith('.exr')])
@@ -59,7 +61,7 @@ input_im_names_outdoor = sorted([f for f in os.listdir(temp_path) if f.endswith(
 
 ## Metrics values
 def fetch_data_metric_values(domaine, method_list_paths, method_label_list, input_im_names):
-    metrics_values_path =Path(f"../Data/Metric_Values/Metrics_Values_{domaine.lower()}/metrics_values_train_{domaine.lower()}.json")
+    metrics_values_path = root / f"../Data/Metric_Values/Metrics_Values_{domaine.lower()}/metrics_values_train_{domaine.lower()}.json"
 
     with open(metrics_values_path) as data_file:    
         harvested_data = json.load(data_file)  
@@ -101,7 +103,7 @@ for domaine in ["Indoor", "Outdoor"]:
         data_metrics_values = fetch_data_metric_values(domaine, method_list_paths, method_label_list, input_im_names)
 
         for material in material_label_list:
-            proportion_preferences_observers_path = Path(f"../Data/Psychophysical_Results/Psychophysical_Results_{domaine.lower()}/{type_of_blender_scenes[type_of_blender_scenes_label_list.index(type_of_blender_scene)]}/{material_list[material_label_list.index(material)]}/proportion_preferences_observers.npy")
+            proportion_preferences_observers_path = root / f"../Data/Psychophysical_Results/Psychophysical_Results_{domaine.lower()}/{type_of_blender_scenes[type_of_blender_scenes_label_list.index(type_of_blender_scene)]}/{material_list[material_label_list.index(material)]}/proportion_preferences_observers.npy"
             moyenne_arr = np.load(proportion_preferences_observers_path)[0]
             moyenne_arr[moyenne_arr < 0.5] = 0
 
